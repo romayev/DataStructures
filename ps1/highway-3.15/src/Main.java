506e01111977be5c406653d0bb06b08ab224f47d
Alex Romayev <romayev@gmail.com>
2017-01-26 16:34:16 -0500

    Setup highways

--- ps1/highway-3.15/src/Main.java
+++ ps1/highway-3.15/src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
 
+    // Create an int array of distances and initialize with values, i.e. 5, 3, 4, 2...
+
     public static void main(String[] args) {
 	// write your code here
+    }
+
+    int calculateDistance(int fromExit, int toExit) {
+        int distance = 0;
+
+        // iterate distances array from 'fromExit' to 'toExit'
+        // sum distances into the 'distance' variable
+
+        return distance;
     }
 }
