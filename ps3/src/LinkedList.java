937cc5b0cf809ccdaf9a8f6cb158ee2cd97887e3
saniik <Sasha@Sanik.fios-router.home>
2017-02-06 18:46:13 -0500

    trying to mess around w delete method

--- ps3/src/LinkedList.java
+++ ps3/src/LinkedList.java
@@ -28,25 +28,81 @@ public class LinkedList {
         return new LinkedList(rear);
     }
 
+//    public boolean delete(String target) {
+//        Node current = rear; //going to use this one to cycle through the list, assuming rear means front
+//        Node prev = null; // set equal to current, have it lag once behind
+//        if (current.data == target){ // in other words if the first node is the target
+//            current = current.next
+//            Node temp = new Node(current.data, current.next.next);
+//                prev = temp;
+//            }
+//            prev = current;
+//            current = current.next;
+//        }
+//        return rear;
+//    }
+
     public boolean delete(String target) {
-          /* COMPLETE THIS METHOD */
-          return false;
-    }
-
-    @Override
-    public String toString() {
-        String result = "";
-        if (rear == null) return result;
-        
-        Node node = rear;
-        while (node.next != rear) {
-            result = result  + node;
-            node = node.next;
+        Node prev = new Node(null,null);
+        if (rear == null) {
+            System.out.println("list is empty");
         }
-        return result;
-    }
 
-    public static void main(String[] args) {
+        Node curr = new Node (null,rear);
+        prev = null;
+
+        while (curr != null){
+            if (curr.data == target){
+                if (prev == null) {
+                    rear = curr.next;
+                }
+                else {
+                    prev = curr;
+                }
+                curr = curr.next
+            }
+            return true;
+        }
+        
+
+//            if (target.equals(rear.data)) {
+//            // found, delete, leaves empty lis
+//                rear = null; return true; }
+//                else { // not found return false; }
+//                 } Node prev=rear, curr=rear.next;
+//        do { if (target.equals(curr.data)) {
+//            prev.next = curr.next;
+//            if (curr == rear) {
+//
+//                //if curr is last node, prev becomes new last node
+//                rear == prev;
+//            }
+//            return true;
+//        } // skip to next node
+//            prev = curr; curr = curr.next; }
+//            while (prev != rear); {
+//                return false; // not found }
+//        }
+//
+//
+
+
+
+            @Override
+            public String toString() {
+                String result = "";
+                if (rear == null) return result;
+
+                Node node = rear;
+                while (node.next != rear) {
+                    result = result  + node;
+                    node = node.next;
+                }
+                return result;
+            }
+
+
+        }public static void main(String[] args) {
         LinkedList list = createList(EXAMPLE_ARRAY);
         System.out.println(list);
         list.delete("red");
