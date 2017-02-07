196ea18b06958bba6850e481fc143c83419333f6
saniik <Sasha@Sanik.fios-router.home>
2017-02-06 20:40:11 -0500

    Perfected Delete method

---
+++ ps3/LinkedList/src/LinkedList.java
@@ -0,0 +1,84 @@
+/**
+ * Created by Sasha on 2/5/17.
+ *
+ * 
+ */
+public class LinkedList {
+    private static final String[] SEASONS = new String[] {"winter", "spring", "summer", "fall"};
+    private static final String[] EMPTY = new String[]{"empty"};
+
+    private Node rear;  // pointer to last node of CLL
+
+    public LinkedList(Node rear) {
+        this.rear = rear;
+    }
+
+    private LinkedList(String[] array) {
+        Node front = null;
+        Node rear = null;
+
+        for (int i = array.length - 1; i >= 0; i-- ) {
+            front = new Node(array[i], front);
+            if (i == array.length - 1) {
+                rear = front;
+            }
+        }
+        if (rear != null) {
+            rear.next = front;
+        }
+        this.rear = rear;
+    }
+
+    @Override
+    public String toString() {
+        if (rear == null) return "{}";
+
+        String result = "";
+        Node first = rear.next;
+        Node node = rear.next;
+        do {
+            result = result + node;
+            node = node.next;
+        } while (node != first);
+
+        return result;
+    }
+
+    public boolean delete(String target) {
+        if (rear == null) {
+            return false;
+        }
+        if (rear.next == rear) {
+            if (rear.data.equals(target)) {
+                rear = null;
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        Node current = rear.next; //going to use this one to cycle through the list, assuming rear means front
+        Node prev = rear;
+
+        while (current != rear) {
+            if (current.data.equals(target)) {
+                prev.next = current.next;
+                return true;
+            }
+            prev = current;
+            current = current.next;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList(EMPTY);
+
+        System.out.println(list);
+        boolean result = list.delete("empty");
+        System.out.println("Result: " + result);
+        System.out.println(list);
+    }
+}
+
