5d3d5d708add2a3d4cb6f60d61c3d685aeebcce6
saniik <Sasha@Sanik.local>
2017-02-07 15:22:43 -0500

    worked on moveToFront

--- ps3/DLLNode/src/Main.java
+++ ps3/DLLNode/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
 
+    public static DLLNode moveToFront(DLLNode front, DLLNode target){
+        if (target == null || front == null){
+            return null;
+        }
+        target.prev.next = target.next; 
+        if (target.next != null){
+            target.next.prev = target.prev;
+        }
+        target.next = front;
+        target.prev = null;
+        front.prev = target;
+        return target; 
+    }
     public static void main(String[] args) {
         
     }
